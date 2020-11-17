39102e51040aa466c95b80a9232ef8f8bc68e4e9
Vegard Joerg <vegard1992@gmail.com>
2020-11-18 00:01:41 +0100

    Setup Part 1

--- Startup.cs
+++ Startup.cs
@@ -1,15 +1,12 @@
-using FontAwesome.Data;
+using Access.Context.Model;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
 
 namespace FontAwesome
 {
@@ -28,7 +25,9 @@ namespace FontAwesome
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
+            services.AddDbContext<FontAwesomeDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("FontAwesomeContext"), b => b.MigrationsAssembly("Access")));
+            services.AddMediatR(typeof(Startup).GetTypeInfo().Assembly);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
